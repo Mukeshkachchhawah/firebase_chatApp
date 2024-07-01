b32c85c20c78dbffdd02977b56dc34876d3ff4d9
deep <mukeshkachhawah49@gmail.com>
2024-07-01 22:13:28 +0530

    modified  chat_view

--- lib/screens/chat_bubbes.dart
+++ lib/screens/chat_bubbes.dart
@@ -1,5 +1,6 @@
 import 'package:chat_application/firebase_provider/firebase_provider.dart';
 import 'package:chat_application/modal/message_modal.dart';
+import 'package:chat_application/ui_helper.dart';
 import 'package:flutter/material.dart';
 
 class ChatBubblesWidget extends StatefulWidget {
@@ -25,53 +26,66 @@ class _ChatBubblesWidgetState extends State<ChatBubblesWidget> {
   Widget fromMsgWidget() {
     var sentTime = TimeOfDay.fromDateTime(
         DateTime.fromMillisecondsSinceEpoch(int.parse(widget.msg.sent)));
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Text('${sentTime.format(context)}'),
-        ),
         Flexible(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Container(
-                margin: const EdgeInsets.all(11),
-                padding: const EdgeInsets.all(11),
-                decoration: BoxDecoration(
-                    color: Colors.amber.shade100,
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(21),
-                        topRight: Radius.circular(21),
-                        bottomLeft: Radius.circular(21))),
-                child: Text(widget.msg.message),
+          child: Container(
+            margin: const EdgeInsets.only(
+                left: 50,
+                right: 10,
+                top: 5,
+                bottom: 5), // Adjust these values as needed
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.amber.shade100,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+                bottomLeft: Radius.circular(12),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Visibility(
-                      visible: widget.msg.read != "",
-                      child: Text(widget.msg.read == ""
-                          ? ""
-                          : TimeOfDay.fromDateTime(
-                                  DateTime.fromMillisecondsSinceEpoch(
-                                      int.parse(widget.msg.read)))
-                              .format(context)
-                              .toString())),
-                  const SizedBox(
-                    width: 7,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Icon(
-                      Icons.done_all_outlined,
-                      color: widget.msg.read != "" ? Colors.blue : Colors.grey,
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Flexible(
+                  child: Text(widget.msg.message),
+                ),
+                wSpace(mWidth: 10),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      '${sentTime.format(context)}',
+                      style: TextStyle(fontSize: 10),
                     ),
-                  ),
-                ],
-              )
-            ],
+                    wSpace(mWidth: 5),
+                    Visibility(
+                        visible: widget.msg.read != "",
+                        child: Text(widget.msg.read == ""
+                            ? ""
+                            : TimeOfDay.fromDateTime(
+                                    DateTime.fromMillisecondsSinceEpoch(
+                                        int.parse(widget.msg.read)))
+                                .format(context)
+                                .toString())),
+                    wSpace(mWidth: 5),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Icon(
+                        Icons.done_all_outlined,
+                        size: 16,
+                        color:
+                            widget.msg.read != "" ? Colors.blue : Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ],
